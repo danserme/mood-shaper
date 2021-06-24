a78ff249e2899c73ac27d6f51d8034e8ec71c148
Polina Bobrova <danserme19@gmail.com>
2021-06-25 01:22:20 +0300

    Color Sliders are made

---
+++ group24/Assets/Scenes/ColorModifier.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorModifier : MonoBehaviour
+{
+    float m_Hue;
+    float m_Saturation;
+    float m_Value;
+    //These are the Sliders that control the values. Remember to attach them in the Inspector window.
+    public Slider m_SliderHue, m_SliderSaturation, m_SliderValue;
+
+    //Make sure your GameObject has a Renderer component in the Inspector window
+    public Renderer m_Renderer;
+
+    void Start()
+    {
+        m_SliderHue.maxValue = 1;
+        m_SliderSaturation.maxValue = 1;
+        m_SliderValue.maxValue = 1;
+
+        m_SliderHue.minValue = 0;
+        m_SliderSaturation.minValue = 0;
+        m_SliderValue.minValue = 0;
+    }
+
+    void Update()
+    {
+        //These are the Sliders that determine the amount of the hue, saturation and value in the Color
+        m_Hue = m_SliderHue.value;
+        m_Saturation = m_SliderSaturation.value;
+        m_Value = m_SliderValue.value;
+
+        //Create an RGB color from the HSV values from the Sliders
+        //Change the Color of your GameObject to the new Color
+        m_Renderer.material.color = Color.HSVToRGB(m_Hue, m_Saturation, m_Value);
+    }
+}
