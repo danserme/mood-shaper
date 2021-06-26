ce429d97c39d723c14cee7e802887fac404adfbf
Polina Bobrova <danserme19@gmail.com>
2021-06-26 18:45:24 +0300

    Num of verts is done

---
+++ group24/Assets/Scenes/Planet.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Planet : MonoBehaviour {
+
+    
+    public Slider s_Res;
+    int resolution = 2;
+
+    [SerializeField, HideInInspector]
+    MeshFilter[] meshFilters;
+    TerrainFace[] terrainFaces;
+
+    void Start()
+    {
+        s_Res.maxValue = 256;
+        
+        s_Res.minValue = 2;
+    }
+
+     void Update()
+    {
+        resolution = (int) s_Res.value;
+        Initialize();
+        GenerateMesh();
+    }
+     
+	private void OnValidate()
+	{
+        Initialize();
+        GenerateMesh();
+	}
+
+	void Initialize()
+    {
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+        terrainFaces = new TerrainFace[6];
+
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");
+                meshObj.transform.parent = transform;
+
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            }
+
+            terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+        }
+    }
+
+    void GenerateMesh()
+    {
+        foreach (TerrainFace face in terrainFaces)
+        {
+            face.ConstructMesh();
+        }
+    }
+}
